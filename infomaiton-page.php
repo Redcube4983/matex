45e6ba347f887d07db5e5ca3ba3c6a2d3b37e7ba
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-08 11:42:04 +0900

    okiyama

---
+++ infomaiton-page.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: infomation-page
+*/
+?>
+<?php get_header(); ?>
+<main>
+<div id="topTtlWrap">
+    <div class="topTtlInner">
+        <div class="topImg"><img src="" alt="トップ背景画像"></div>
+        <div class="ttlWrap">
+            <h2 class="enttl">About us</h2>  
+        </div>
+    </div>
+</div>
+<div id="bcList">
+    <ul class="list">
+        <li>
+            <a href="/">TOP</a>
+        </li>
+        <li>
+            <span>企業情報</span>
+        </li>
+    </ul>
+</div>
+
+</main>
+<?php get_footer(); ?>
