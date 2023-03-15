e2e6229c86545678d16246ab9f4c19a2ba6dc724
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-15 16:56:30 +0900

    koki

--- businessBase-page.php
+++ businessBase-page.php
@@ -49,7 +49,6 @@ Template Name: businessBase
             <div class="mapWrap">
                 <div class="map">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.58176607823!2d139.78592561568001!3d35.71190833589544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188ebddfb1f479%3A0x91b13bf4ca75affb!2z44CSMTExLTAwMzYg5p2x5Lqs6YO95Y-w5p2x5Yy65p2-44GM6LC377yR5LiB55uu77yR77yQ4oiS77yS!5e0!3m2!1sja!2sjp!4v1678866915844!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </iframe>
                 </div>
                 <div class="addressWrap">
                     <h3>本社</h3>
