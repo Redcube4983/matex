c37e6a8649e078226bd164f66b005671948fa8b5
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-15 17:24:30 +0900

    oki

--- businessBase-page.php
+++ businessBase-page.php
@@ -97,7 +97,46 @@ Template Name: businessBase
                 </div>
             </div>
         </li>
-        
+        <li>
+            <div class="baseImg">
+                <picture class="">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-sp.jpg 2x">
+                    <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-pc.jpg 2x">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-pc.jpg 2x" alt="拠点画像">
+                </picture>
+            </div>
+            <div class="mapWrap">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3215.7754254959764!2d139.05825471569025!3d36.29349860375607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601eecc03df97c1b%3A0x37951147530c3f00!2z44CSMzcwLTEyMDEg576k6aas55yM6auY5bSO5biC5YCJ6LOA6YeO55S677yT77yS77yQ77yQIOODnuODhuODg-OCr-OCuee-pOmmrA!5e0!3m2!1sja!2sjp!4v1678868596912!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="addressWrap">
+                    <h3>群馬工場／営業所</h3>
+                    <h4>〒370-1201<br>群馬県高崎市倉賀野町3200</h4>
+                    <span class="number">Tel. 027-346-0808　Fax. 027-346-3123</span>
+                    <span class="property">敷地 4,300㎡<br>事務所 384㎡<br>工場 1,780㎡</span>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="baseImg">
+                <picture class="">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-sp.jpg 2x">
+                    <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-pc.jpg 2x">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-pc.jpg 2x" alt="拠点画像">
+                </picture>
+            </div>
+            <div class="mapWrap">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3232.983242001022!2d139.43864921568274!3d35.87392752698627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018d96499cd35cf%3A0x25f68500d9f061a5!2z44CSMzUwLTEzMzEg5Z-8546J55yM54ut5bGx5biC5paw54ut5bGx77yR5LiB55uu77yR77yR4oiS77yW!5e0!3m2!1sja!2sjp!4v1678868643815!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="addressWrap">
+                    <h3>埼玉工場／営業所</h3>
+                    <h4>〒350-1331<br>埼玉県狭山市新狭山1-11-6</h4>
+                    <span class="number">Tel. 04-2954-0808　Fax. 04-2954-3208</span>
+                    <span class="property">敷地 1,849㎡<br>事務所 116㎡<br>工場 1,000㎡</span>
+                </div>
+            </div>
+        </li>
     </ul>
 </section>
     
