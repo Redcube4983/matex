a40f466bf4fc052a79f3bbcf5026a531687195a1
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-15 15:41:39 +0900

    oki

--- businessBase-page.php
+++ businessBase-page.php
@@ -15,8 +15,8 @@ Template Name: businessBase
                 </picture>
         </div>
         <div class="ttlWrap">
-            <span class="enTtl">Company profile</span>
-            <h1 class="jpTtl">会社概要</h1>
+            <span class="enTtl">Business base</span>
+            <h1 class="jpTtl">事業拠点</h1>
         </div>
     </div>
 </div>
@@ -26,10 +26,83 @@ Template Name: businessBase
             <a href="<?php echo esc_url( home_url( '/') ); ?>">TOP</a>
         </li>
         <li>
-            <span>会社概要</span>
+            <span>事業拠点</span>
         </li>
     </ul>
-</div>
+</div> 
+<section class="leadWrap">
+        <div class="leadTtlWrap">
+            <h2 class="jpTtl">お客様に最高の満足をお届けするために</h2>
+        </div>
+        <p class="">東京本社を中心に、関東から東北にかけて、生産と販売サービスを一体化した拠点をバランスよく展開。<br>お客様のさまざまなニーズにタイムリーにお応えできる体制を整えています。</p>
+</section>
+<section id="base">
+    <ul class="baseInner">
+        <li>
+            <div class="baseImg">
+                <picture class="">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-sp.jpg 2x">
+                    <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-pc.jpg 2x">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-pc.jpg 2x" alt="拠点画像">
+                </picture>
+            </div>
+            <div class="mapWrap">
+                <div class="mapImg">
+                    <img src="/images/businessBase/map01-sp.jpg" alt="地図画像">
+                </div>
+                <div class="addressWrap">
+                    <h3>本社</h3>
+                    <h4>〒111-0036<br>東京都台東区松が谷1-10-2</h4>
+                    <span class="number">Tel. 03-3844-0808　Fax. 03-3844-4109</span>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="baseImg">
+                <picture class="">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-sp.jpg 2x">
+                    <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-pc.jpg 2x">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-pc.jpg 2x" alt="拠点画像">
+                </picture>
+            </div>
+            <div class="mapWrap">
+                <div class="mapImg">
+                    <img src="/images/businessBase/map01-sp.jpg" alt="地図画像">
+                </div>
+                <div class="addressWrap">
+                    <h3>栃木工場／営業所</h3>
+                    <h4>〒327-0816<br>栃木県佐野市栄町佐野工業団地14街区1画地</h4>
+                    <span class="number">Tel. 0283-23-0808　Fax. 0283-24-1145</span>
+                    <span class="property">敷地 14,500㎡　<br class="tab pc pc2">事務所 1,372 ㎡　<br class="tab pc pc2">工場 6,474㎡</span>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="baseImg">
+                <picture class="">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-sp.jpg 2x">
+                    <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-pc.jpg 2x">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-pc.jpg 2x" alt="拠点画像">
+                </picture>
+            </div>
+            <div class="mapWrap">
+                <div class="mapImg">
+                    <img src="/images/businessBase/map01-sp.jpg" alt="地図画像">
+                </div>
+                <div class="addressWrap">
+                    <h3>福島工場／営業所</h3>
+                    <h4>〒969-1301<br>福島県安達郡大玉村大山字向原大玉村第一工業団地</h4>
+                    <span class="number">Tel. 0243-48-2008　Fax. 0243-48-3678</span>
+                    <span class="property">敷地 18,000㎡ 　<br class="tab pc pc2">事務所 300㎡ 　<br class="tab pc pc2">工場 3,880㎡</span>
+                </div>
+            </div>
+        </li>
+        
+    </ul>
+</section>
     
+    
+
+
 </main>
 <?php get_footer(); ?>
