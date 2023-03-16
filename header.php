8d752a1cc9df1097e4c03222ca7c60c133aa3b76
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-16 17:07:09 +0900

    oki

--- header.php
+++ header.php
@@ -11,25 +11,16 @@
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/common/images/apple-touch-icon.png">
 <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/common/images/apple-touch-icon.png">
 <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/common/images/favicon.ico">
+
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/common/css/magnific-popup.css">
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet">
-<?php if( is_front_page() ): ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page.css">
-<?php if( is_page( 'message' ) ): ?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-aboutus.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-message.css">
-<?php if( is_page( 'profile' ) ): ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-profile.css">
-<?php if( is_page( 'businessBase' ) ): ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-businessBase.css">
-<?php if( is_page( 'quality' ) ): ?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-business.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-quality.css">
-<?php if( is_page( ) ): ?>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page.css">
-<?php else: ?>
-<?php endif; ?>
-
-
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&display=swap" rel="stylesheet">
