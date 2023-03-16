810489afac93cb1ad65f00837d8db23fff5e956c
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-16 16:46:34 +0900

    oki

--- header.php
+++ header.php
@@ -18,10 +18,10 @@
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/common/css/magnific-popup.css">
 <?php if( is_front_page() ): ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page.css">
-<?php elseif( is_page( 'aboutus' ) ): ?>
+<!--
+<?php elseif( is_page( ) ): ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page.css">
-<?php elseif( is_page( 'business' ) ): ?>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page.css">
+-->
 <?php elseif( is_page( 'message' ) ): ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-message.css">
 <?php elseif( is_page( 'profile' ) ): ?>
