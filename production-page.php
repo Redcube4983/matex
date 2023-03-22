8afb789549e89b8e4bfdc64476c8084aace23a25
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-22 13:25:17 +0900

    oki

---
+++ production-page.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: production
+*/
+?>
+<?php get_header(); ?>
+<main id="mainWrap">
+<div id="topTtlWrap">
+    <div class="topTtlInner">
+        <div class="topImg">
+            <picture class="">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/business/production/production-top-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/business/production/production-top-sp.jpg 2x">
+                    <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/business/production/production-top-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/business/production/production-top-pc.jpg 2x">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/business/production/production-top-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/business/production/production-top-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/business/production/production-top-pc.jpg 2x" alt="トップ背景画像">
+                </picture>
+        </div>
+        <div class="ttlWrap">
+            <span class="enTtl">Network</span>
+            <h1 class="jpTtl">販売/納品体制</h1>
+        </div>
+    </div>
+</div>
+<div id="panNavi">
+    <ul class="list">
+        <li>
+            <a href="<?php echo esc_url( home_url( '/') ); ?>">TOP</a>
+        </li>
+        <li>
+            <a href="/business/">事業情報</a>
+        </li>
+        <li>
+            <span>生産体制</span>
+        </li>
+    </ul>
+</div>
+<section id="leadWrap">
+        <div class="leadTtlWrap">
+            <h2 class="jpTtl">“拠点主義”体制から<br class="sp">ジャストインタイムでデリバリー</h2>
+        </div>
+</section>
+
+</main>
+<?php get_footer(); ?>
+
