d61495b7460f75b516c3c5de2ca8efac7892fa3b
“matex” <m-kawabe@matex-cc.co.jp>
2023-03-29 17:50:50 +0900

    oki

---
+++ officeJob-page.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Template Name: officejob
+*/
+?>
+<?php get_header(); ?>
+<main id="mainWrap">
+<div id="topTtlWrap">
+    <div class="topTtlInner">
+        <div class="topImg">
+            <picture class="">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/recruit/work/officeJob/officeJob-top-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/recruit/work/officeJob/officeJob-top-sp.jpg 2x">
+                    <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/recruit/work/officeJob/officeJob-top-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/recruit/work/officeJob/officeJob-top-pc.jpg 2x">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/recruit/work/officeJob/officeJob-top-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/recruit/work/officeJob/officeJob-top-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/recruit/work/officeJob/officeJob-top-pc.jpg 2x" alt="トップ背景画像">
+                </picture>
+        </div>
+        <div class="ttlWrap">
+            <span class="enTtl">Office Job</span>
+            <h1 class="jpTtl">営業職</h1>
+        </div>
+    </div>
+</div>
+<div id="panNavi">
+    <ul class="list">
+        <li>
+            <a href="<?php echo esc_url( home_url( '/') ); ?>">TOP</a>
+        </li>
+        <li>
+            <a href="/recruit/">事業情報</a>
+        </li>
+        <li>
+            <span>仕事を知る</span>
+        </li>
+    </ul>
+</div>
+<section id="leadWrap">
+        <div class="leadTtlWrap">
+            <h2 class="jpTtl">“拠点主義”体制から<br class="sp">ジャストインタイムでデリバリー</h2>
+        </div>
+</section>
+<section id="workInner">
+    
+</section>
+
+</main>
+<?php get_footer(); ?>
+
+
+
+
+
+
+
+
