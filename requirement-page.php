b3a79d2ee19883caa252ecd8a3794981f2657fec
“matex” <m-kawabe@matex-cc.co.jp>
2023-04-03 15:54:27 +0900

    oki

--- requirement-page.php
+++ requirement-page.php
@@ -75,10 +75,10 @@ Template Name: requirement
             $free_item = SCF::get_option_meta('theme-options', 'idealProfile');
             foreach ($free_item as $fields) { 
 
-            if($fields['about'] !== ""){
+            if($fields['profile'] !== ""){
             ?>
 
-            <li>・<?php echo $fields['about']; ?></li>
+            <li>・<?php echo $fields['profile']; ?></li>
 
             <?php
             } 
