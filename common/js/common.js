bbc78bbcc592dd9705b1c07b93840d575da8782d
“matex” <m-kawabe@matex-cc.co.jp>
2023-04-04 11:58:07 +0900

    oki

--- common/js/common.js
+++ common/js/common.js
@@ -230,7 +230,7 @@ $(window).on('scroll',function(){
 
 $(function(){
     $('a[href^="#"]').click(function(){
-        var speed = 500;
+        var speed = 300;
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
         var position = target.offset().top;
