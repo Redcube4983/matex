074924b2fcc70ba73d7476a45a4ae5f77d08d0dc
Redcube4983 <98586827+Redcube4983@users.noreply.github.com>
2023-04-26 17:00:24 +0900

    nagasaka修正

--- front-page.php
+++ front-page.php
@@ -109,7 +109,8 @@ Template Name: front-page
         </div>
     </div>
 </section>
-    <div class="link-image-area" class='animate-bottom'>
+<section id="link-area"　class='animate-bottom'>
+    <div class="link-image-area">
      <img class="link-bg" src="<?php echo get_template_directory_uri(); ?>/images/front-page/link-bg.jpg" alt="">
     </div>
     <div class="link-area">
