0e1bf353471b16a76e3bb85121ada58134ce484e
“matex” <m-kawabe@matex-cc.co.jp>
2023-04-26 17:04:38 +0900

    oki

--- footer.php
+++ footer.php
@@ -52,7 +52,6 @@
                             </li>
                         </ul>
                 </li>
-                <li class="gran-child"><h4><a href="/work/">仕事を知る</a></h4></li>
                 <li class="gran-child"><h4><a href="/requirement/">募集要項</a></h4></li>
             </ul>
         </li>
