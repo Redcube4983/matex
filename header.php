ee3ac822069999a42310edd84b394b5cb543ba7b
“matex” <m-kawabe@matex-cc.co.jp>
2023-04-27 14:56:28 +0900

    oki

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 <?php elseif(is_page(array('officejob', 'technical', 'salesjob'))) : ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page2.css">
 <?php elseif( is_page( 'news' ) ): ?>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-news.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/single-news.css">
 <?php elseif( is_page( 'message' ) ): ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/page-message.css">
 <?php elseif( is_page( 'profile' ) ): ?>
