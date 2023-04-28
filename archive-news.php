1f29e21467f6f088210e2e73a642acd688389945
“matex” <m-kawabe@matex-cc.co.jp>
2023-04-28 15:48:41 +0900

    oki

--- archive-news.php
+++ archive-news.php
@@ -36,8 +36,9 @@ Template Name: archive-news
 <ul>
 <?php if (have_posts()): ?>
     <?php while (have_posts()) : the_post(); ?>
-    <li class="topics-inner"><time class="topics-time"><?php echo get_the_date('Y/m/d'); ?></time><p class="topics-text"><a class="topics-link" href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></p></li> 
+    <li class="news-inner"><time class="news-time"><?php echo get_the_date('Y/m/d'); ?></time><p class="news-text"><a class="news-link" href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></p></li> 
     <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
     <?php else: ?>
     <!-- 投稿が無い場合の処理 -->
     <?php endif; ?> 
