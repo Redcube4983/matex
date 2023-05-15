b9bd9fc99eec0527ddc9e08d0979b5071488e909
Redcube4983 <98586827+Redcube4983@users.noreply.github.com>
2023-05-15 16:03:52 +0900

    nagasaka修正

--- page-network.php
+++ page-network.php
@@ -34,7 +34,7 @@ Template Name: network
     </ul>
 </div>
 <section id="leadWrap">
-        <div class="leadTtlWrap">
+        <div class="leadTtlWrap animate-bottom">
             <h2 class="jpTtl">“拠点主義”体制から<br class="sp">ジャストインタイムでデリバリー</h2>
         </div>
 </section>
@@ -43,38 +43,38 @@ Template Name: network
     <div class="textWrap">
         <ul>
             <li>
-                <h3>販売体制</h3>
-                <p>鉄鋼メーカーから供給される一次製品を、お客様のニーズに合わせて加工、納品するのが私たちコイルセンターの役割です。
+                <h3 class="animate-bottom">販売体制</h3>
+                <p class="animate-bottom">鉄鋼メーカーから供給される一次製品を、お客様のニーズに合わせて加工、納品するのが私たちコイルセンターの役割です。
                     マテックスは、北関東から東北にかけて、生産と販売サービスを一体化した拠点をバランスよく展開。東京本社と各拠点が緻密に連携し、最適な在庫管理、納期管理を実現しています。お客様の身近にあって、その声をじかに聞き、最短の反応でニーズにお応えする。この「拠点主義」こそマテックスの一貫したポリシーです。私たちはこれからもお客様と親密で永続的な信頼関係を守るために最善を尽くします。</p>
             </li>
             <li>
-                <h3>納品体制</h3>
-                <p>ジャストインタイムの納品体制を支えているのは、マテックス専属の輸送会社、カンテツ運輸株式会社による強力な配送システムです。カンテツ運輸はもともとマテックスの輸送部門が分離独立したもので、24トントレーラーや大型トラックを多数保有し、万全な配送システムを構築しています。コイル輸送専門に長い経験をつんだ運行スタッフがマテックスの4拠点それぞれに常駐し、さまざまな状況に臨機応変に対応しながら、第二の営業マンとして誠実にお客様の元へお届けしています。</p>
+                <h3 class="animate-bottom">納品体制</h3>
+                <p class="animate-bottom">ジャストインタイムの納品体制を支えているのは、マテックス専属の輸送会社、カンテツ運輸株式会社による強力な配送システムです。カンテツ運輸はもともとマテックスの輸送部門が分離独立したもので、24トントレーラーや大型トラックを多数保有し、万全な配送システムを構築しています。コイル輸送専門に長い経験をつんだ運行スタッフがマテックスの4拠点それぞれに常駐し、さまざまな状況に臨機応変に対応しながら、第二の営業マンとして誠実にお客様の元へお届けしています。</p>
             </li>
         </ul>
-        <div class="mapImg">
+        <div class="mapImg animate-fade">
             <img src="<?php echo get_template_directory_uri(); ?>/images/business/network/networkImg01.svg" alt="地図画像">
         </div>
     </div>
     </div> 
     <div class="imgWrap">
-        <div class="position">
+        <div class="position animate-bottom">
             <img src="<?php echo get_template_directory_uri(); ?>/images/business/network/networkImg02.svg" alt="体制仕組み図画像">
         </div>
         <div class="stateImgWrap">
         <div class="stateImgWrap1">
-            <div class="img1">
+            <div class="img1 animate-bottom">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/business/network/networkImg03.jpg" alt="様子画像">
             </div>
-            <div class="img2">
+            <div class="img2 animate-bottom">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/business/network/networkImg04.jpg" alt="様子画像">
             </div>
         </div>
         <div class="stateImgWrap2">
-            <div class="img3">
+            <div class="img3 animate-bottom">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/business/network/networkImg05.jpg" alt="様子画像">
             </div>
-            <div class="img4">
+            <div class="img4 animate-bottom">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/business/network/networkImg06.jpg" alt="様子画像">
             </div>
         </div>
