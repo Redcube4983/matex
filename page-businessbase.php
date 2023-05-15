bcb1ade54a0104ccb072842ed09c7c87ab1f162c
Redcube4983 <98586827+Redcube4983@users.noreply.github.com>
2023-05-15 15:56:22 +0900

    nagasaka調整

--- page-businessbase.php
+++ page-businessbase.php
@@ -34,15 +34,15 @@ Template Name: businessbases
     </ul>
 </div> 
 <section id="leadWrap">
-        <div class="leadTtlWrap">
+        <div class="leadTtlWrap animate-bottom">
             <h2 class="jpTtl">お客様に最高の満足をお届けするために</h2>
         </div>
-        <p class="textCenter">東京本社を中心に、関東から東北にかけて、生産と販売サービスを一体化した拠点をバランスよく展開。<br class="pc">お客様のさまざまなニーズにタイムリーにお応えできる体制を整えています。</p>
+        <p class="textCenter animate-bottom">東京本社を中心に、関東から東北にかけて、生産と販売サービスを一体化した拠点をバランスよく展開。<br class="pc">お客様のさまざまなニーズにタイムリーにお応えできる体制を整えています。</p>
 </section>
 <section id="base">
     <ul class="baseInner">
         <li>
-            <div class="baseImg">
+            <div class="baseImg animate-bottom">
                 <picture class="">
                     <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-sp.jpg 2x">
                     <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base01-pc.jpg 2x">
@@ -50,10 +50,10 @@ Template Name: businessbases
                 </picture>
             </div>
             <div class="mapWrap">
-                <div class="map">
+                <div class="map animate-fade">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.58176607823!2d139.78592561568001!3d35.71190833589544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188ebddfb1f479%3A0x91b13bf4ca75affb!2z44CSMTExLTAwMzYg5p2x5Lqs6YO95Y-w5p2x5Yy65p2-44GM6LC377yR5LiB55uu77yR77yQ4oiS77yS!5e0!3m2!1sja!2sjp!4v1678866915844!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <div class="addressWrap">
+                <div class="addressWrap animate-bottom">
                     <h3>本社</h3>
                     <h4>〒111-0036<br>東京都台東区松が谷1-10-2</h4>
                     <span class="number">Tel. 03-3844-0808　<br class="pc">Fax. 03-3844-4109</span>
@@ -61,7 +61,7 @@ Template Name: businessbases
             </div>
         </li>
         <li>
-            <div class="baseImg">
+            <div class="baseImg animate-bottom">
                 <picture class="">
                     <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-sp.jpg 2x">
                     <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base02-pc.jpg 2x">
@@ -69,10 +69,10 @@ Template Name: businessbases
                 </picture>
             </div>
             <div class="mapWrap">
-                <div class="map">
+                <div class="map animate-fade">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3215.0012431223704!2d139.60681611569075!3d36.312276702710996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601f3a29fad3010b%3A0x34f8ead36fc15d65!2z5L2Q6YeO5bel5qWt5Zuj5Zyw57eP5ZCI566h55CG5Y2U5Lya!5e0!3m2!1sja!2sjp!4v1678867446677!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <div class="addressWrap">
+                <div class="addressWrap animate-bottom">
                     <h3>栃木工場／営業所</h3>
                     <h4>〒327-0816<br>栃木県佐野市栄町佐野工業団地14街区1画地</h4>
                     <span class="number">Tel. 0283-23-0808　<br class="pc">Fax. 0283-24-1145</span>
@@ -81,7 +81,7 @@ Template Name: businessbases
             </div>
         </li>
         <li>
-            <div class="baseImg">
+            <div class="baseImg animate-bottom">
                 <picture class="">
                     <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-sp.jpg 2x">
                     <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base03-pc.jpg 2x">
@@ -89,10 +89,10 @@ Template Name: businessbases
                 </picture>
             </div>
             <div class="mapWrap">
-                <div class="map">
+                <div class="map animate-fade">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3163.188536894924!2d140.39811157181083!3d37.5506215947344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6020747ab861759d%3A0x511067c331bc237!2z77yI5qCq77yJ44Oe44OG44OD44Kv44K5IOemj-WztuW3peWgtOWWtualreaJgA!5e0!3m2!1sja!2sjp!4v1678867503382!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <div class="addressWrap">
+                <div class="addressWrap animate-bottom">
                     <h3>福島工場／営業所</h3>
                     <h4>〒969-1301<br>福島県安達郡大玉村大山字向原大玉村第一工業団地</h4>
                     <span class="number">Tel. 0243-48-2008　<br class="pc">Fax. 0243-48-3678</span>
@@ -101,7 +101,7 @@ Template Name: businessbases
             </div>
         </li>
         <li>
-            <div class="baseImg">
+            <div class="baseImg animate-bottom">
                 <picture class="">
                     <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-sp.jpg 2x">
                     <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base04-pc.jpg 2x">
@@ -109,10 +109,10 @@ Template Name: businessbases
                 </picture>
             </div>
             <div class="mapWrap">
-                <div class="map">
+                <div class="map animate-fade">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3215.7754254959764!2d139.05825471569025!3d36.29349860375607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601eecc03df97c1b%3A0x37951147530c3f00!2z44CSMzcwLTEyMDEg576k6aas55yM6auY5bSO5biC5YCJ6LOA6YeO55S677yT77yS77yQ77yQIOODnuODhuODg-OCr-OCuee-pOmmrA!5e0!3m2!1sja!2sjp!4v1678868596912!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <div class="addressWrap">
+                <div class="addressWrap animate-bottom">
                     <h3>群馬工場／営業所</h3>
                     <h4>〒370-1201<br>群馬県高崎市倉賀野町3200</h4>
                     <span class="number">Tel. 027-346-0808　<br class="pc">Fax. 027-346-3123</span>
@@ -121,7 +121,7 @@ Template Name: businessbases
             </div>
         </li>
         <li>
-            <div class="baseImg">
+            <div class="baseImg animate-bottom">
                 <picture class="">
                     <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-sp.jpg 2x">
                     <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-pc.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/businessBase/base05-pc.jpg 2x">
@@ -129,10 +129,10 @@ Template Name: businessbases
                 </picture>
             </div>
             <div class="mapWrap">
-                <div class="map">
+                <div class="map animate-fade">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3232.983242001022!2d139.43864921568274!3d35.87392752698627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018d96499cd35cf%3A0x25f68500d9f061a5!2z44CSMzUwLTEzMzEg5Z-8546J55yM54ut5bGx5biC5paw54ut5bGx77yR5LiB55uu77yR77yR4oiS77yW!5e0!3m2!1sja!2sjp!4v1678868643815!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <div class="addressWrap">
+                <div class="addressWrap animate-bottom">
                     <h3>埼玉工場／営業所</h3>
                     <h4>〒350-1331<br>埼玉県狭山市新狭山1-11-6</h4>
                     <span class="number">Tel. 04-2954-0808　<br class="pc">Fax. 04-2954-3208</span>
