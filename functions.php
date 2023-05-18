933533b017cc402967cf993729faf21804a24757
Redcube4983 <98586827+Redcube4983@users.noreply.github.com>
2023-05-18 14:34:33 +0900

    fanctions変更

--- functions.php
+++ functions.php
@@ -28,6 +28,48 @@ function change_posts_per_page($query) {
     }
    }
    add_action( 'pre_get_posts', 'change_posts_per_page' );
+
+
+// ログイン画面 ロゴ変更
+function login_logo_image() {
+    echo '<style type="text/css">
+            #login h1 a {
+                background: url(' . get_bloginfo('template_directory') . '/common/images/matex-logo.svg) no-repeat;
+                width: 100%;
+                background-size: 100% auto;
+                height: 90px;
+            }
+    </style>';
+}
+add_action('login_head', 'login_logo_image');
+
+// アイキャッチを有効化
+add_theme_support( 'post-thumbnails' );
+
+/* 投稿内の画像を相対パスに */
+function delete_domain_from_attachment_url( $url ) {
+ if ( preg_match( '/^http(s)?:\/\/[^\/\s]+(.*)$/', $url, $match ) ) {
+ $url = $match[2];
+ }
+ return $url;
+}
+add_filter( 'wp_get_attachment_url', 'delete_domain_from_attachment_url' );
+
+/* 投稿内の画像を相対パスに */
+function imagepassshort($arg) {
+$content = str_replace('"images/', '"' . get_bloginfo('template_directory') .'/images/', $arg);
+return $content;
+}
+add_action('the_content', 'imagepassshort');
+
+/* 投稿内のショートコード有効 */
+function my_php_Include($params = array()) {
+    extract(shortcode_atts(array('file' => 'default'), $params));
+    ob_start();
+    include(STYLESHEETPATH . "/$file.php");
+    return ob_get_clean();
+}
+add_shortcode('php', 'my_php_Include');
 ?>
 
 
