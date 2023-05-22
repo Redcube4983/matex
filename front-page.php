53f9b0648e7a7c29827c0e222e7d1016d3775eac
“matex” <m-kawabe@matex-cc.co.jp>
2023-05-22 18:17:49 +0900

    oki

--- front-page.php
+++ front-page.php
@@ -71,7 +71,7 @@ Template Name: front-page
 </section>
 <section id="movie-area" class="animate-bottom">
 <div class="movie-text-area">
-    <a class="movie-buttun popup-movie" href="https://www.youtube.com/watch?v=KyziIy5xKUI">
+    <a class="movie-buttun popup-movie" href="https://www.youtube.com/embed/KyziIy5xKUI">
     <span></span>
     </a>
     <div class="movie-text-area-top">
