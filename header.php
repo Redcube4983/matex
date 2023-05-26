16577bda4db7ca9578e39fa24b7889d397591a4f
Redcube4983 <98586827+Redcube4983@users.noreply.github.com>
2023-05-26 14:55:31 +0900

    googleGA追加

--- header.php
+++ header.php
@@ -65,7 +65,15 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <?php wp_head(); ?>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z57K9JHKBZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-Z57K9JHKBZ');
+</script>
 <body>
 
 <header id="headerWrap">
