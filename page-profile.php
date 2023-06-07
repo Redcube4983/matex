e5d4b9445f30744358a3a246b28ebac3acf1b82d
“matex” <m-kawabe@matex-cc.co.jp>
2023-06-07 11:39:37 +0900

    oki

--- page-profile.php
+++ page-profile.php
@@ -9,9 +9,9 @@ Template Name: profile
     <div class="topTtlInner">
         <div class="topImg">
             <picture class="">
-                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-sp.jpg 1x,<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-sp.jpg 2x">
+                    <source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-sp.webp 1x,<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-sp.webp 2x">
                     <source media="(max-width: 920px)" srcset="<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-pc.webp 1x,<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-pc.webp 2x">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-sp.jpg" srcset="<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-pc.webp 1x,<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-pc.webp 2x" alt="トップ背景画像">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-sp.webp" srcset="<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-pc.webp 1x,<?php echo get_template_directory_uri(); ?>/images/aboutus/profile/profile-top-pc.webp 2x" alt="トップ背景画像">
                 </picture>
         </div>
         <div class="ttlWrap">
